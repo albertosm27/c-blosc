16add1bc6c5888cf8bae7f46c6842e45d1e04b5e
Alberto Sabater <alberto3pt@gmail.com>
2017-06-20 12:26:15 +0200

    Update run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,11 +3,21 @@
 set -e
 set -x
 
-if [[ "$(uname -s)" == 'Darwin' ]]; then
-    if which pyenv > /dev/null; then
-        eval "$(pyenv init -)"
+if [ -z "$TRAVIS_TAG" ]
+    then
+        export CONAN_REFERENCE="c-blosc/${TRAVIS_TAG}"
+        if [[ "$(uname -s)" == 'Darwin' ]]; then
+            if which pyenv > /dev/null; then
+                eval "$(pyenv init -)"
+            fi
+            pyenv activate conan
+        fi
+        python build.py
+    else
+        mkdir build
+        cd build
+        cmake ..
+        cmake --build . --config Release
+        ctest
     fi
-    pyenv activate conan
-fi
-pwd
-python build.py
+
