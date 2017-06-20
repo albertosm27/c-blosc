ca52582589d478e6e94f1c59d8569a65b3b8340a
Alberto Sabater <alberto3pt@gmail.com>
2017-06-20 12:57:53 +0200

    Update conanfile.py

--- test_package/conanfile.py
+++ test_package/conanfile.py
@@ -4,11 +4,11 @@ import os
 
 channel = os.getenv("CONAN_CHANNEL", "testing")
 username = os.getenv("CONAN_USERNAME", "albertosm27")
-
+version = os.environ['CONAN_REFERENCE'].split('/')[1]
 
 class CbloscTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    requires = "c-blosc/0.2@%s/%s" % (username, channel)
+    requires = "c-blosc/%s@%s/%s" % (version, username, channel)
     generators = "cmake"
 
     def imports(self):
