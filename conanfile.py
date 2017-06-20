24c9e663c367ddb8ca646636cb0a62e48bc8ce6b
Alberto Sabater <alberto3pt@gmail.com>
2017-06-20 09:06:22 +0200

    Update conanfile.py

--- conanfile.py
+++ conanfile.py
@@ -29,7 +29,8 @@ class CbloscConan(ConanFile):
         self.run("cmake --build . %s" % cmake.build_config)
 
     def package(self):
-        self.copy("*.h", dst="include", src="c-blosc/blosc")
+        self.copy("blosc.h", dst="include", src="c-blosc/blosc")
+        self.copy("blosc-export.h", dst="include", src="c-blosc/blosc")
         self.copy("*.lib", dst="lib", src="lib", keep_path=False)
         self.copy("*.dll", dst="bin", src="bin", keep_path=False)
         self.copy("*.dylib", dst="bin", src="bin", keep_path=False)
