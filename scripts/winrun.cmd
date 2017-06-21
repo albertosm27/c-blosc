f6ac9e5ca2e4d6f3d373582ae05e65e3baa879af
Alberto Sabater <alberto3pt@gmail.com>
2017-06-21 11:06:31 +0200

    Update winrun.cmd

--- scripts/winrun.cmd
+++ scripts/winrun.cmd
@@ -1,11 +1,11 @@
-IF "%APPVEYOR_REPO_TAG_NAME%"=="" (          
-    SET CONAN_REFERENCE="c-blosc/%APPVEYOR_REPO_TAG_NAME%"
-    python build.py
-) ELSE (
+IF "%APPVEYOR_REPO_TAG_NAME%"=="" (
     rmdir /q /s _builds
     mkdir _builds
     cmake "-G%GENERATOR%" -B_builds
     cmake --build _builds --config "%CONFIG%"
     cd _builds
     ctest -VV -C "%CONFIG%"
+) ELSE (
+    SET CONAN_REFERENCE="c-blosc/%APPVEYOR_REPO_TAG_NAME%"
+    python build.py
 )
