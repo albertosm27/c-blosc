87c1f4df5bfe09e08bd63b7dccfee7ad126956d8
Alberto Sabater <alberto3pt@gmail.com>
2017-06-21 11:10:50 +0200

    Update winrun.cmd

--- scripts/winrun.cmd
+++ scripts/winrun.cmd
@@ -1,7 +1,5 @@
 IF "%APPVEYOR_REPO_TAG_NAME%"=="" (
-    rmdir /q /s _builds
-    mkdir _builds
-    cmake "-G%GENERATOR%" -B_builds
+    cmake "-G%GENERATOR%" -H. -B_builds
     cmake --build _builds --config "%CONFIG%"
     cd _builds
     ctest -VV -C "%CONFIG%"
